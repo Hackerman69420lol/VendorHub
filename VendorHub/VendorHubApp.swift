d1765925f7dcc041d26b93e646c51ead03df25ae
Hackerman69420 <mikedccs06@gmail.com>
2023-12-18 15:35:34 -0500

    Initial Commit

---
+++ VendorHub/VendorHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  VendorHubApp.swift
+//  VendorHub
+//
+//  Created by Michael Washington on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct VendorHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
